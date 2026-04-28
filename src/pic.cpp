0b8f3feb0523df66d4d4c9fe7bd5e1331baddf83
Snow <snowdm00@gmail.com>
2026-04-28 18:32:20 -0500

    fix: explicitly unmask keyboard interrupt in PIC

--- src/pic.cpp
+++ src/pic.cpp
@@ -19,10 +19,7 @@ void pic_send_eoi(unsigned char irq) {
 }
 
 void pic_remap(int offset1, int offset2) {
-    unsigned char a1, a2;
-
-    a1 = inb(PIC1_DATA);                        // save masks
-    a2 = inb(PIC2_DATA);
+    // Limine masks all interrupts by default. We don't want to restore those.
 
     outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
     io_wait();
@@ -44,8 +41,9 @@ void pic_remap(int offset1, int offset2) {
     outb(PIC2_DATA, ICW4_8086);
     io_wait();
 
-    outb(PIC1_DATA, a1);   // restore saved masks.
-    outb(PIC2_DATA, a2);
+    // Unmask IRQ1 (Keyboard) on Master PIC. Mask all others.
+    outb(PIC1_DATA, 0xFD); // 1111 1101
+    outb(PIC2_DATA, 0xFF); // 1111 1111
 }
 
 void pic_disable(void) {
